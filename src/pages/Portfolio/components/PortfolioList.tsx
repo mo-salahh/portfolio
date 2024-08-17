8758590988e3035b529587d5408b9d6276aa0d6a
Mohammad Salah <m.salah@alifbee.com>
2024-08-17 13:05:09 +0300

    fix swiper and protfolio modal

--- src/pages/Portfolio/components/PortfolioList.tsx
+++ src/pages/Portfolio/components/PortfolioList.tsx
@@ -1,63 +1,70 @@
-import React, { useState } from 'react';
-// import { Swiper, SwiperSlide } from 'swiper/react';
-import PortfolioItem from './PortfolioItem';
-import PortfolioModal from './PortfolioModal';
-import { PortfolioItem as PortfolioItemType} from '../types';
-
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "../../../swiper/swiper-bundle.min.css"; // Import Swiper styles
+import { Navigation, Pagination } from "swiper/modules"; // Updated import path
+import PortfolioItem from "./PortfolioItem";
+import PortfolioModal from "./PortfolioModal";
+import { PortfolioItem as PortfolioItemType } from "../types";
 
 interface PortfolioListProps {
-    items: PortfolioItemType[];
+  items: PortfolioItemType[];
 }
 
 const PortfolioList: React.FC<PortfolioListProps> = ({ items }) => {
-    const [activeModal, setActiveModal] = useState<number | null>(null);
+  const [activeModal, setActiveModal] = useState<number | null>(null);
 
-    const handleViewMore = (id: number) => {
-        setActiveModal(id);
-    };
+  const handleViewMore = (id: number) => {
+    setActiveModal(id);
+  };
 
-    const handleCloseModal = () => {
-        setActiveModal(null);
-    };
+  const handleCloseModal = () => {
+    setActiveModal(null);
+  };
 
-    return (
-        <div className="portfolio__container container swiper-container">
-            {/* <Swiper spaceBetween={30} slidesPerView={1} navigation pagination={{ clickable: true }}> */}
-                {items.map((item) => (
-                    // <SwiperSlide key={item.id}>
-                        <PortfolioItem
-                            imgSrc={item.imgSrc}
-                            title={item.title}
-                            description={item.description}
-                            githubLink={item.githubLink}
-                            onViewMore={() => handleViewMore(item.id)}
-                        />
-                    // </SwiperSlide>
-                ))}
-            {/* </Swiper> */}
+  return (
+    <div className="portfolio__container container">
+      <Swiper
+        modules={[Navigation, Pagination]}
+        navigation={{ nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' }}
+        pagination={{ clickable: true }}
+        className="swiper-container"
+      >
+        {items.map((item) => (
+          <SwiperSlide key={item.id}>
+            <PortfolioItem
+              imgSrc={item.imgSrc}
+              title={item.title}
+              description={item.description}
+              githubLink={item.githubLink}
+              onViewMore={() => handleViewMore(item.id)}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
 
-            {/* Swiper Navigation Buttons */}
-            <div className="swiper-button-next">
-                <i className="uil uil-angle-right-b swiper-portfolio-icon"></i>
-            </div>
-            <div className="swiper-button-prev">
-                <i className="uil uil-angle-left-b swiper-portfolio-icon"></i>
-            </div>
-            <div className="swiper-pagination"></div>
+      {/* Swiper Navigation Buttons */}
+      <div className="swiper-button-next">
+        <i className="uil uil-angle-right-b swiper-portfolio-icon"></i>
+      </div>
+      <div className="swiper-button-prev">
+        <i className="uil uil-angle-left-b swiper-portfolio-icon"></i>
+      </div>
+      <div className="swiper-pagination"></div>
 
-            {/* Modal Content */}
-            {items.map((item) => (
-                activeModal === item.id && (
-                    <PortfolioModal
-                        key={item.id}
-                        title={item.title}
-                        modalContent={item.modalContent}
-                        onClose={handleCloseModal}
-                    />
-                )
-            ))}
-        </div>
-    );
+      {/* Modal Content */}
+      {items.map(
+        (item) =>
+          activeModal === item.id && (
+            <PortfolioModal
+              key={item.id}
+              title={item.title}
+              modalContent={item.modalContent}
+              onClose={handleCloseModal}
+            />
+          )
+      )}
+    </div>
+  );
 };
 
 export default PortfolioList;
--- src/pages/Portfolio/components/PortfolioModal.tsx
+++ src/pages/Portfolio/components/PortfolioModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 interface PortfolioModalProps {
     title: string;
@@ -7,9 +7,25 @@ interface PortfolioModalProps {
 }
 
 const PortfolioModal: React.FC<PortfolioModalProps> = ({ title, modalContent, onClose }) => {
+
+    const modalRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+                onClose();
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [onClose]);
+
     return (
-        <div className="portfolio__modal">
-            <div className="portfolio__modal-content">
+        <div className="portfolio__modal active-modal">
+            <div className="portfolio__modal-content" ref={modalRef}>
                 <h4 className="portfolio__modal-title">{title}</h4>
                 <i className="uil uil-times portfolio__modal-close" onClick={onClose}></i>
                 <ul className="portfolio__modal-portfolio grid">
